69256bb04728bb705136d25cdb1c167a2f1c6b47
zyadelashwal <zyadelashwal32@gmail.com>
2022-05-08 19:23:49 +0200

    remove unuse code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo"/>
+        <img src={logo} alt="logo" className="App-logo"/>
         
       </header>
       <body>
